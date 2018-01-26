50917c3a2eeae4cc0d49e9b32400a6f043a9d00c
Hanio90 <hanio90@gmail.com>
2018-01-25 20:08:24 -0500

    This is my LAB5

---
+++ LAB5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB5
+
+{ public class DiceRollerApp
+    {
+        public static int RollingDice(int x)
+        {
+            Random rnum = new Random();
+            int die1 = rnum.Next(1, x);
+            int die2 = rnum.Next(1, x);
+
+            return die1 + die2;
+        }
+    }  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool confirm = true;
+
+            Console.WriteLine("Hello welcome to the Grand Circus Casino! \n\nWould you like to ROLL? Y/N");
+            string response = Console.ReadLine();
+
+            if (response == "Y" || response == "y")
+            {
+                while (confirm == true)
+                {
+                    Console.WriteLine("Enter the number of sides for a pair of dice");
+                    int x = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine();
+
+                    Console.WriteLine(DiceRollerApp.RollingDice(x));
+
+                    int rolled = DiceRollerApp.RollingDice(x);
+
+                    if (rolled == 2)
+                    {
+                        Console.WriteLine("SNAKE EYES!");
+                    }
+                    else if(rolled == 7)
+                    {
+                        Console.WriteLine("CRAPS OHH NOOOOOO!");
+                    }
+                    else if (rolled == 11)
+                    {
+                        Console.WriteLine("CRAPS OHH NOOOOOO!");
+                    }
+                    else if (rolled ==12 )
+                    {
+                        Console.WriteLine("BOX CARS!");
+                    }
+                    Console.WriteLine("Would you like to roll again ? Y/N");
+                    response = Console.ReadLine();
+
+                    if (response == "y" || response == "Y")
+
+                        continue;
+                    else
+                        confirm = false;
+                }
+            }
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
